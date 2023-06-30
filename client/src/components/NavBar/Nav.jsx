62eeef88ac8cd9f0f75ecb379ab113a95820d61f
Juan SB <juansb.dev@gmail.com>
2023-06-30 18:54:21 -0300

    sizes done

--- client/src/components/NavBar/Nav.jsx
+++ client/src/components/NavBar/Nav.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import styles from './Nav.module.css'
 import { addCharacter, getCharacters } from "../../redux/actions";
+import NavRight from "./NavRight"; 
+
 
 
 
@@ -9,45 +11,16 @@ export default function Nav(){
     const dispatch = useDispatch();
     const characters = useSelector(state => state.characters)
 
-let pokemonTitle = 'https://pokemon-shiny.vercel.app/title.png'
-
-    const [id, setId ] = useState('Buscar Pokemon')
-
-    const handleInput = (e)=>{
-        let {value} = e.target;
-        setId(value)
-        console.log(value)
-    }
-
-    const buscarCharacter = ()=>{
-          dispatch(addCharacter(id));
-          setId('Buscar Pokemon');
-        }
+    let pokemonTitle = 'https://pokemon-shiny.vercel.app/title.png'
         
-    let pokeBola = 'https://img.freepik.com/free-icon/pokeball_318-196468.jpg';
-
-        useEffect(()=>{
-            dispatch(getCharacters())
-},[dispatch])
 
     return(
         <div className={styles.container}>
             <div className={styles.containerLeft}>
                 <img src={pokemonTitle} alt="" />
             </div>
-            <div className={styles.ContainerRight}>
-                <input className={styles.input}
-                    placeholder={id}
-                    type='search'
-                    name='search'
-                    onChange={handleInput}
-                />
-                <button className={styles.button_buscar}
-                onClick={()=> buscarCharacter(id)}
-                >
-                <img src={pokeBola} alt="" />
-                </button>
-            </div>
+
+            <NavRight className={styles.ContainerRight} />  
             
         </div>
     )
---
+++ client/src/components/NavBar/NavRight.jsx
@@ -0,0 +1,60 @@
+import React, {useState,useEffect} from 'react'
+import styles from './NavRight.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { addCharacter, getCharacters } from "../../redux/actions";
+
+let pokeBola = 'https://img.freepik.com/free-icon/pokeball_318-196468.jpg';
+
+
+
+
+
+
+export default function NavRight() {
+
+    const dispatch = useDispatch();
+
+
+    const [id, setId ] = useState('Buscar Pokemon')
+    
+    const handleInput = (e)=>{
+        let {value} = e.target;
+        setId(value)
+        console.log(value)
+    }
+    
+    const buscarCharacter = ()=>{
+        dispatch(addCharacter(id));
+        setId('Buscar Pokemon');
+    }
+    
+    useEffect(()=>{
+        dispatch(getCharacters())
+},[dispatch])
+    
+  return (
+            <div className={styles.container}>
+                <div className={styles.buscador}>
+                        <input className={styles.input}
+                            placeholder={id}
+                            type='search'
+                            name='search'
+                            onChange={handleInput}
+                        />
+                        <button className={styles.button_buscar}
+                                onClick={()=> buscarCharacter(id)}
+                        >
+                        <img src={pokeBola} alt=""
+                        />
+                        </button>
+                </div>
+                <div className={styles.botonera}>
+                        <p>favorites</p>
+                        <p>Crear</p>
+                </div>
+
+            </div>
+            
+            )
+}
