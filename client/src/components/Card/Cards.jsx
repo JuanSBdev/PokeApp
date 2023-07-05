804f2842bce3e69fb584f2b5fd375fb291becf94
Juan SB <juansb.dev@gmail.com>
2023-07-05 17:09:24 -0300

    filtros

--- client/src/components/Card/Cards.jsx
+++ client/src/components/Card/Cards.jsx
@@ -1,73 +1,106 @@
-import React from 'react';
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Card from './Card';
-import styles from './Cards.module.css'
+import styles from './Cards.module.css';
+
 export default function Cards(props) {
-   const  character  = useSelector( state => state.characters);
+   const [currentPage, setCurrentPage] = useState(1);
+   
+   const character = useSelector(state => state.characters);
+   
+   const cardsPerPage = 12;
+   const indexOfLastCard = currentPage * cardsPerPage;
+   const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+   // Ordenar
 
-    const cardsPerPage = 12;
-    const [currentPage, setCurrentPage] = useState(1);
-    // Calcula el índice inicial y final de las tarjetas que se mostrarán en la página actual
-    const indexOfLastCard = currentPage * cardsPerPage;
-    const indexOfFirstCard = indexOfLastCard - cardsPerPage;
-    const currentCards = character.slice(indexOfFirstCard, indexOfLastCard);
-  
-    // Calcula el número total de páginas
-    const totalPages = Math.ceil(character.length / cardsPerPage);
-  
-    // Cambia a la página siguiente
-    const nextPage = () => {
-      setCurrentPage(prevPage => prevPage + 1);
-    };
-  
-    // Cambia a la página anterior
-    const previousPage = () => {
-      if( currentPage >= 2){
 
-         setCurrentPage(prevPage => prevPage - 1);
+   const [sortBy, setSortBy] = useState('original'); 
+   const currentCards = character
+    .slice(indexOfFirstCard, indexOfLastCard)
+    .sort((a, b) => {
+      if (sortBy === 'nombre') {
+        return a.nombre.localeCompare(b.nombre);
+      } else {
+        // como estaba originalmente
+        return 0;
       }
-     
-    };
+    });
   
 
-    return (<div className={styles.Cards}>
-      
-         <div className={`${styles.paginacion} `} >
-               {currentPage >= 2 && (
-                <button   onClick={previousPage}>
-                        <span className="material-symbols-outlined" >
-                           arrow_back
-                        </span>
-               </button>
-               )}
-                 
-              {currentPage < totalPages && (
-                <button onClick={nextPage}>
-                  <span className="material-symbols-outlined">
-                     arrow_forward
-                  </span>   
-               </button>
-              )}
-              
-               
-         </div>
+  const totalPages = Math.ceil(character.length / cardsPerPage);
 
-         <div className={styles.card}>
-         { character[0] && 
-         currentCards.map((character, index) => (<Card
-            key={index} id={character.id}
-            name={character.nombre}
-            imagen ={character.imagen}
-            vida ={character.vida}
-            ataque = { character.ataque}
-            habilidad = {character.habilidad}
-            tipos = {character.tipo[0]}
-         >
-            </Card>))
-      }
+  const nextPage = () => {
+    setCurrentPage(prevPage => prevPage + 1);
+  };
 
-         </div>
+  const previousPage = () => {
+    if (currentPage >= 2) {
+      setCurrentPage(prevPage => prevPage - 1);
+    }
+  };
 
-         </div>);
+  const handleSortByNombre = () => {
+    if (sortBy !== 'nombre') {
+      setSortBy('nombre');
+    }
+  };
+
+  const handleSortByOriginal = () => {
+    if (sortBy !== 'original') {
+      setSortBy('original');
+    }
+  };
+
+  return (
+    <div className={styles.Cards}>
+      <div className={`${styles.paginacion} `}>
+        {currentPage >= 2 && (
+          <button onClick={previousPage}>
+            <span className="material-symbols-outlined">arrow_back</span>
+          </button>
+        )}
+
+        {currentPage < totalPages && (
+          <button onClick={nextPage}>
+            <span className="material-symbols-outlined">arrow_forward</span>
+          </button>
+        )}
+      </div>
+
+      <div className={styles.filters}>
+        <button
+          onClick={handleSortByOriginal}
+          className={`${styles.filterButton} ${
+            sortBy === 'original' ? styles.active : ''
+          }`}
+        >
+          Original
+        </button>
+        <button
+          onClick={handleSortByNombre}
+          className={`${styles.filterButton} ${
+            sortBy === 'nombre' ? styles.active : ''
+          }`}
+        >
+          Nombre
+        </button>
+      </div>
+
+      <div className={styles.card}>
+        {character[0] &&
+          currentCards.map((character, index) => (
+            <Card
+              key={index}
+              id={character.id}
+              name={character.nombre}
+              imagen={character.imagen}
+              vida={character.vida}
+              ataque={character.ataque}
+              habilidad={character.habilidad}
+              tipos={character.tipo[0]}
+            ></Card>
+          ))}
+      </div>
+    </div>
+  );
 }
